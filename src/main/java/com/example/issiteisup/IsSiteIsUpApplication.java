77306b5331ec81c1c30a3c9bedc185b64ed85a28
rakeshdyx <rakeshdyx@gmail.com>
2021-01-20 23:00:38 +0530

    testsitestatus

---
+++ src/main/java/com/example/issiteisup/IsSiteIsUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.issiteisup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsSiteIsUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsSiteIsUpApplication.class, args);
+	}
+
+}
